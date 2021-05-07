b6049cf13f0720d22173a690f8847747b9f3b745
cyberkingcj <57757541+cyberkingcj@users.noreply.github.com>
2021-05-07 17:08:02 +0530

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -1,7 +1,20 @@
-pincode = "458001"  #Do not remove quotes while changing the pincode
+pincode = ""  #Do not remove quotes while changing the pincode
 
-t = 30 
+t = 60
 #Change the above value to customize time frame(in seconds). Don't make it below 30 to avoid DDOS
 
-age = 18 
+age = 45 
 #Change the above value to set age. Set 18 for 18-44 and 45 for 45+.
+
+name = ""
+#Change it to the name of person who is in your recent chat list on Whatsapp
+#Enter exactly the same name else it'll fail 
+
+pings = 10
+#Change it to the number of times you want to ping the person when vaccine gets available
+
+interval_between_pings = 10
+#The interval between consecutive pings in seconds. It's recommended to keep it above 5
+
+path_to_chromedriver = r"C:\Users\CJ\Desktop\chromedriver"
+#Change this to wherever you have put the chromedriver file.Do not remove r and quotes
